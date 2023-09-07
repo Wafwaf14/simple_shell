abce0a17e252ccfa88c69f04309677282dd2c44f
Wafwaf14 <wafaa.elhijary@gmail.com>
2023-09-07 04:59:03 -0700

    test

---
+++ wafaa/separfunction.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/*
+ *
+ *
+ *
+ */
+
+int main(int ac, char **av)
+{
+	(void)ac ;
+	int i = 0;
+	char *buffer = NULL;
+	size_t size_buffer = 0;
+	int num_char =0;
+
+	while (av[i])
+	{
+		printf("%s \n" , av[i]);
+		i++;
+	}
+	printf("$ ");
+	num_char = getline(&buffer, &size_buffer, stdin);
+	 if (num_char == EOF)
+		 perror("getlone");
+	 printf("%s\n",buffer);
+	  return (0);
+}
+
