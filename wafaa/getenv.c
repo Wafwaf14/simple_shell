bb33adf8ee258ba73fcfcd8b7cc8fc3c60acc39e
Wafaa HIJARY <95526772+Wafwaf14@users.noreply.github.com>
2023-09-11 10:39:58 +0100

    Update getenv.c

--- wafaa/getenv.c
+++ wafaa/getenv.c
@@ -1,60 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// function can 
+// function can return the envairment element
 char *_getenv(const char *env_var)
 {
 	extern char **environ;
 	int i = 0;
 	char *key;
+	// in this loop we will check every environ , when we get the string that we need we will print the value of it 
 	while (environ[i])
 	{
 		key = strtok(environ[i] , "=");
 		if (strcmp(env_var,key) == 0 )
-			return(strtok(NULL, "\n"));
+			return(strtok(NULL, "="));
 		i++;
 	}
 	return (NULL);
 }
-	char *get_command(char *command)
-	{
-		char *path = _getenv("PATH");
-		char *token ;
-		char *cmd_full ;
-		struct stat st;
-
-		token = strlen(pat, ":");
-		while (token)
-		{
-			cmd_full = malloc(strlen(token) + strlen(command) + 2)
-			       strcpy(cmd_full , token );
-		strcat(cmd_full , '/');
-		strcat(cmd_full, command);
-		if (stat(cmd_full , &st) == 0)
-			return (cmd_full);
-		free(cmd_full);
-		token =strtok(NULL, ":");
-		}
-		return (NULL);
-	}
-
-int main(int ac , char **av, char **env)
-{
-	char *buffer = NULL;
-	size_t buffer_size = 0;
-	char *cmd;
-	char **args;
-	pid_t pid ;
-	int status, nchars;
-
-	while (1);
-	{
-		write(1,"$ ", 2);
-		nchars = getline(&buffer, &buffer_size, stdin);
-		if(nchars == -1)
-		{
-			write(1,"\n",1);
-			exit(1);
-		}
-	}
+int main()
+{// return the path 
+	printf("%s\n",_getenv("PATH"));
+	return(0);
 }
