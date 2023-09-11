60ab2570916e22e66fda911e4308fdb45ef7d881
Wafaa HIJARY <95526772+Wafwaf14@users.noreply.github.com>
2023-09-11 10:24:52 +0100

    Update env.c

--- wafaa/env.c
+++ wafaa/env.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+// environ vs env 
 int main(int ac , char **av, char **env)
 {
-	int i = 0;
+//to do array 
+	 int i = 0;
 	while (env[i])
 	{
 		printf("%s\n", env[i]);
 		i++ ;
 	}
+	// global varibal 
 	extern char **environ ;
+	// print the adress for env 
 	printf("%p\n" ,env);
+	// printing the adreess for environ for commpaire if are the same or not 
 	printf("%p\n",environ);
 }
